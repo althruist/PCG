bb29c3a520738db47fc0232d966aa1283ce4e237
Kieran <73611627+althruist@users.noreply.github.com>
2026-05-09 18:30:55 +0200

    player movement and animations, camera effects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,20 @@ public class Player : MonoBehaviour
     private DungeonGenerator dungeonGenerator;
 
 
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 3f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
+    private SpriteRenderer sprite;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         dungeonGenerator = FindFirstObjectByType<DungeonGenerator>();
 
         Vector2Int spawnPos = dungeonGenerator.SpawnRoomCenter;
-        transform.position = new Vector3(spawnPos.x, spawnPos.y, 0);
+        transform.position = new Vector3(spawnPos.x + 0.5f, spawnPos.y + 0.5f, 0);
     }
 
     private void Update()
@@ -24,6 +27,44 @@ public class Player : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         movement = movement.normalized;
+        if (movement == new Vector2(0, 0))
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsWalkingUp", false);
+            animator.SetBool("IsWalkingDown", false);
+        }
+        else
+        {
+            if (movement == new Vector2(-1, 0))
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                animator.SetBool("IsWalking", true);
+                animator.SetBool("IsWalkingUp", false);
+                animator.SetBool("IsWalkingDown", false);
+            }
+            else if (movement == new Vector2(1, 0))
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                animator.SetBool("IsWalking", true);
+                animator.SetBool("IsWalkingUp", false);
+                animator.SetBool("IsWalkingDown", false);
+
+            }
+            else if (movement == new Vector2(0, 1))
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                animator.SetBool("IsWalkingUp", true);
+                animator.SetBool("IsWalkingDown", false);
+                animator.SetBool("IsWalking", false);
+            }
+            else if (movement == new Vector2(0, -1))
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                animator.SetBool("IsWalkingDown", true);
+                animator.SetBool("IsWalkingUp", false);
+                animator.SetBool("IsWalking", false);
+            }
+        }
     }
 
     private void FixedUpdate()
